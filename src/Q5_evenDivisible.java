cc7b87b446de748e3cdef123e3d7a49491cdb240
anand-prashar <anand.dv4@gmail.com>
2014-12-01 19:27:38 +0530

    Create Q5_evenDivisible.java
    work upload

---
+++ src/Q5_evenDivisible.java
@@ -0,0 +1,16 @@
+
+public class Q5_evenDivisible 
+{
+	public static void main(String[] args) 
+	{
+		Q5_evenDivisible o=new Q5_evenDivisible();
+		o.solution();
+
+	}
+	
+	void solution()
+	{
+		
+	}
+
+}
